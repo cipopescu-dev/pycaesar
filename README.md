b653fa57dac807907b840632f348805eecd91f82
Ciprian Popescu <popescu.ciprian98@gmail.com>
2024-02-02 15:55:17 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# pycaesar
+
+```bash
+usage: caesar.py [-h] [-i INPUT] [-k KEY] [-o OUTPUT] [-d]
+
+options:
+  -h, --help            show this help message and exit
+  -i INPUT, --input INPUT
+  -k KEY, --key KEY
+  -o OUTPUT, --output OUTPUT
+  -d, --decrypt
+```
+```bash
+python3 caesar.py -i input.txt -k key.txt 
+Input: Imi place sa mananc
+Output: Iam dpegi se aebebg
+```
---
+++ caesar.py
@@ -0,0 +1,60 @@
+import argparse
+import sys
+
+
+def doCaesar(text, shifts, alphabet):
+    result = ""
+    alphabet_len = len(alphabet)
+    if len(set(alphabet)) != alphabet_len:
+        print("Error: the given alphabet has duplicate letters")
+        sys.exit(1)
+    alphabet_map = {char: i for i, char in enumerate(alphabet)}
+    for char in text:
+        if char in alphabet_map:
+            original_index = alphabet_map[char]
+            new_index = (original_index + shifts) % alphabet_len
+            result += alphabet[new_index]
+        else:
+            result += char
+    return result
+
+
+def argParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", type=str)
+    parser.add_argument("-k", "--key", type=str)
+    parser.add_argument("-o", "--output", type=str)
+    parser.add_argument("-d", "--decrypt", action="store_true")
+    return parser
+
+
+def readInputs(input, key):
+    with open(input) as inputf, open(key) as keyf:
+        shifts = int(keyf.readline())
+        alphabet = keyf.readline()
+        text = inputf.read()
+        return dict(text=text, shifts=shifts, alphabet=alphabet)
+
+
+def writeToFile(text, filename):
+    with open(filename, "w") as outputf:
+        outputf.write(text)
+
+
+def main():
+    parser = argParser()
+    args = parser.parse_args()
+    if not (args.input and args.key):
+        parser.print_help()
+        sys.exit(1)
+
+    text, shifts, alphabet = readInputs(args.input, args.key).values()
+    if args.decrypt:
+        shifts = -shifts
+    output = doCaesar(text, shifts, alphabet)
+    print(f"Input: {text}\nOutput: {output}")
+    writeToFile(output, args.output or "output.txt")
+
+
+if __name__ == "__main__":
+    main()
